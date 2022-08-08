5502d3cac42a9e6d6117ecce95f84383b16e0458
Nalle12 <nallelyalfaro02@gmail.com>
2022-08-08 15:58:41 -0700

    Modificaciones al Proyecto

---
+++ Proyecto/src/main/java/com/proyectoWeb/controller/carrosController.java
@@ -0,0 +1,9 @@
+package com.proyectoWeb.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class carrosController {
+    // ACA TIENEN QUE HACER TODA LA LOGICA DE LOS CONTROLADORES 
+    // PARA LOS CARROS Y MANDARLOS A LLAMAR
+}
