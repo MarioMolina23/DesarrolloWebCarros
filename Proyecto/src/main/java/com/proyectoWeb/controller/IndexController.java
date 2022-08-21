e4e21aa2762a8ff7d953cab8a8be33d41d03c038
MarioMolina23 <94089210+MarioMolina23@users.noreply.github.com>
2022-08-21 01:30:18 -0600

    Jasper Report

--- Proyecto/src/main/java/com/proyectoWeb/controller/IndexController.java
+++ Proyecto/src/main/java/com/proyectoWeb/controller/IndexController.java
@@ -1,11 +1,22 @@
 package com.proyectoWeb.controller;
 
+import com.proyectoWeb.service.AudiReportService;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class indexController {
     
+    @Autowired
+    private AudiReportService audiReportService;
+        
     @GetMapping("/index")
     public String inicio() {
         return "index";
@@ -85,4 +96,21 @@ public class indexController {
         return "/cliente/MetodoPago";
     }
 
+    @GetMapping(value = "/ReporteAudis", produces = MediaType.APPLICATION_PDF_VALUE)
+    public @ResponseBody
+    byte[] getFile() throws IOException {
+        try {
+            FileInputStream fis = new FileInputStream(new File(audiReportService.generateReport()));
+            byte[] targetArray = new byte[fis.available()];
+            fis.read(targetArray);
+            return targetArray;
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
