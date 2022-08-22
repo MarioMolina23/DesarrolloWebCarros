16692995f30d4822deb7d52eb98924eb12a46e3a
MarioMolina23 <94089210+MarioMolina23@users.noreply.github.com>
2022-08-21 23:40:07 -0600

    Agregue toda la internacionalización a la página y mejore vistas de login, register y métodos de pago

--- Proyecto/src/main/java/com/proyectoWeb/SecurityConfig.java
+++ Proyecto/src/main/java/com/proyectoWeb/SecurityConfig.java
@@ -28,13 +28,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/administrador/vehiculos", "/administrador/modificarVehiculo"
+                .antMatchers("/administrador/vehiculos", "/administrador/modificarVehiculo",
+                        "/administrador/comentarios"
                 )
                     .hasRole("ADMIN")
                 .antMatchers("/cliente/acercaDe", "/cliente/contacto", "/cliente/galeria","/cliente/login",
                         "/cliente/register", "/cliente/terminosCondiciones", "/carros/audi", "/carros/bmw",
                         "/carros/honda", "/carros/mitsubishi", "/carros/toyota", "/carros/volvo",
-                        "/comprar/compra", "/administrador/vehiculos"
+                        "/comprar/compra", "/cliente/MetodoPago", "/nuevoComentario", "/index", "/index(lang=es)",
+                        "/index(lang=en)"
                 )
                     .hasAnyRole("ADMIN","CLIENTE")
                 .antMatchers("/")
